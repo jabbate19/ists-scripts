62c8623129803925d2de952b66ec3188a30be0d7
Joe Abbate <40615740+jabbate19@users.noreply.github.com>
2023-03-01 23:10:30 -0500

    Update theranos.sh

--- scripts/theranos.sh
+++ scripts/theranos.sh
@@ -2,8 +2,8 @@
 curl http://skillissue.fyi/steeloxide -o steeloxide
 curl http://skillissue.fyi/theranos.json -o theranos.json
 ./steeloxide preset --config theranos.json
-echo "pass out proto { tcp udp } from 10.12.1.4 to 192.168.12.0/24" >> pf.conf
-echo "pass in proto { tcp udp } from 192.168.12.0/24 to 10.12.1.4 " >> pf.conf
-echo "pass out proto { tcp udp } from 10.12.1.8 to 192.168.12.0/24" >> pf.conf
-echo "pass in proto { tcp udp } from 192.168.12.0/24 to 10.12.1.8 " >> pf.conf
-pfctl -F rules -f pf.conf
+echo "pass out proto { tcp udp } from 10.12.1.4 to 192.168.12.0/24" >> /etc/pf.conf
+echo "pass in proto { tcp udp } from 192.168.12.0/24 to 10.12.1.4 " >> /etc/pf.conf
+echo "pass out proto { tcp udp } from 10.12.1.8 to 192.168.12.0/24" >> /etc/pf.conf
+echo "pass in proto { tcp udp } from 192.168.12.0/24 to 10.12.1.8 " >> /etc/pf.conf
+pfctl -F rules -f /etc/pf.conf
